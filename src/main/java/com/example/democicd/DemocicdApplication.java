dd2b25dd1f4ecc9dccfbeb2143cbc553a855c2ab
oshan <oshxn998@gmail.com>
2024-05-13 15:47:20 +0530

    initial commit

---
+++ src/main/java/com/example/democicd/DemocicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.democicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocicdApplication.class, args);
+	}
+
+}
